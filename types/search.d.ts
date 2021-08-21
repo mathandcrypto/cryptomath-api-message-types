beae63471153c4757e7d92df046c8bf4a70430d0
Viktor Schastnyy <victor92sh@gmail.com>
2021-08-21 22:51:16 +0300

    Updated search message types

--- types/search.d.ts
+++ types/search.d.ts
@@ -14,7 +14,26 @@ export interface InsertHubDocumentRequest {
     description: string;
 }
 
+export interface UpdateHubStatsRequest {
+    documentId: string;
+    articlesCount: number;
+    tagsCount: number;
+}
+
+export interface InsertTagDocumentRequest {
+    id: number;
+    hubId: number;
+    hubDocumentId: string;
+    name: string;
+    description: string;
+}
+
 export interface InsertDocumentResponse {
     isDocumentCreated: boolean;
     documentId?: string;
+}
+
+export interface UpdateDocumentResponse {
+    isDocumentUpdated: boolean;
+    version?: number;
 }
